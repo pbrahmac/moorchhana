07fd1c23d0e44284a9994975efc61cc5ee35e3ee
Parjanya Brahmachari <pbrahmac@gmail.com>
2023-10-11 01:55:21 -0700

    Compact mode updates

--- src/lib/components/KeyboardNotes.svelte
+++ src/lib/components/KeyboardNotes.svelte
@@ -1,44 +1,61 @@
 <script lang="ts">
 	import { Toggle } from '$lib/components/ui/toggle';
-	import { noteMap } from '$lib/utils';
+	import { allNotes, noteMap } from '$lib/utils';
+	import { getContext } from 'svelte';
+	import type { Writable } from 'svelte/store';
 
 	// props
 	export let selectedNotes: string[];
 
+	// compact mode store (from Context API)
+	const compactModeStore: Writable<boolean> = getContext('compactMode');
+
 	const notes = [...noteMap.entries()].map((arr) => ({ note: arr[0], properties: arr[1] }));
 </script>
 
-<div class="flex flex-col rounded-lg lg:px-4 py-2 aspect-video max-w-xs lg:max-w-full">
-	<div class="flex">
-		{#each notes as { note, properties }, idx}
-			{#if properties.row === 'top'}
-				<Toggle
-					disabled
-					pressed={selectedNotes.includes(note)}
-					class="border-2 w-8 h-20 disabled:opacity-100 items-end"
-				>
-					{note}
-				</Toggle>
-			{:else if notes[idx].properties.row === 'bottom' && notes[idx - 1].properties.row === 'bottom'}
-				<div class="w-4 ml-4 h-20 border-b-2" />
-			{:else}
-				<div class="w-4 h-20 border-b-2" />
-			{/if}
+{#if $compactModeStore}
+	<div class="flex flex-wrap items-center justify-center gap-1 rounded-lg px-3 py-2">
+		{#each allNotes as note}
+			<Toggle disabled class="disabled:opacity-100" pressed={selectedNotes.includes(note)}
+				>{note}</Toggle
+			>
 		{/each}
 	</div>
-	<div class="flex">
-		{#each notes as { note, properties }}
-			{#if properties.row === 'bottom'}
-				<Toggle
-					disabled
-					pressed={selectedNotes.includes(note)}
-					class="border-2 border-t-0 rounded-t-none w-8 h-20 disabled:opacity-100 items-end"
-				>
-					{note}
-				</Toggle>
-			{:else}
-				<div class="first:w-6 w-4 h-20" />
-			{/if}
-		{/each}
+{:else}
+	<div class="flex flex-col rounded-lg lg:px-4 py-2 aspect-video max-w-xs lg:max-w-full">
+		<div class="flex">
+			{#each notes as { note, properties }, idx}
+				{#if properties.row === 'top'}
+					<Toggle
+						disabled
+						variant="outline"
+						pressed={selectedNotes.includes(note)}
+						class="w-8 h-20 disabled:opacity-100 items-end"
+					>
+						{note}
+					</Toggle>
+				{:else if notes[idx].properties.row === 'bottom' && notes[idx - 1].properties.row === 'bottom'}
+					<div class="w-4 ml-4 h-20 border-b-2" />
+				{:else}
+					<div class="w-4 h-20 border-b-2" />
+				{/if}
+			{/each}
+		</div>
+		<div class="flex">
+			{#each notes as { note, properties }}
+				{#if properties.row === 'bottom'}
+					<Toggle
+						disabled
+						variant="outline"
+						pressed={selectedNotes.includes(note)}
+						class="border-t-0 rounded-t-none w-8 h-20 disabled:opacity-100 items-end"
+					>
+						{note}
+					</Toggle>
+				{:else}
+					<div class="first:w-6 w-4 h-20" />
+				{/if}
+			{/each}
+		</div>
 	</div>
-</div>
+{/if}
